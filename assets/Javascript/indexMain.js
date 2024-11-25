d3d932804ea89b435249a71e6e824dd049c76f21
CaioSViana <47906580+CaioSViana@users.noreply.github.com>
2024-11-24 22:50:33 -0300

    Atualização visual / css

--- assets/Javascript/indexMain.js
+++ assets/Javascript/indexMain.js
@@ -1,3 +1,4 @@
+// Swiper
 var swiper = new Swiper(".mySwiper", {
 	slidesPerView: 4,
 	spaceBetween: 30,
@@ -7,3 +8,36 @@ var swiper = new Swiper(".mySwiper", {
 	},
 	loop: true
   });
+
+//botão pesquisas
+let btn = document.querySelectorAll('.btn-pesquisar');
+let pesquisar = document.querySelectorAll('.itempesquisa');
+
+btn.forEach((button) => {
+	button.addEventListener('click', () => {
+		pesquisar.forEach((item) => item.classList.contains('active') ? item.classList.remove('active') : null);
+		button.parentElement.classList.add('active');
+  });
+});
+
+
+
+
+// Botão projeto futuro brilhante - opções
+let cols = document.querySelectorAll('.colx');
+cols.forEach((col) => {
+  col.addEventListener('click', () => {
+    cols.forEach((item) => item.classList.contains('active') ? item.classList.remove('active') : null);
+    col.classList.add('active');
+  });
+});
+
+
+// Botão empresa-projeto
+let buttonm = document.querySelectorAll('.button-more');
+buttonm.forEach((button) => {
+	button.addEventListener('click', () => {
+	buttonm.forEach((item) => item.classList.contains('active') ? item.classList.remove('active') : null);
+    button.classList.add('active');
+  });
+});
