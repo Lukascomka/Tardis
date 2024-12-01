80fbbd922bfa8e74c57aed6238b7b0a522bda54b
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-12-01 01:49:15 -0300

    responsividade

--- assets/Javascript/menu.js
+++ assets/Javascript/menu.js
@@ -1,8 +1,8 @@
 document.querySelector('.hamburger').addEventListener('click', function () {
     this.classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
-    document.querySelector('.logo-wrapper').classList.toggle('active'); // Controla a visibilidade da logo-wrapper
-    document.querySelector('.logo-container').classList.toggle('active'); // Controla a visibilidade da logo
+//     document.querySelector('.logo-wrapper').classList.toggle('active'); 
+//     document.querySelector('.logo-container').classList.toggle('active'); 
 });
 
 
